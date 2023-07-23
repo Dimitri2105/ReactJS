bc9b19acf5eac2936e4bbbef783479281d18a849
Dimitri2105 <karanthakur577@gmail.com>
2023-07-23 07:22:02 +0530

    Adding Event Listeners made easy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,6 +5,12 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const clickHandeler = () =>{
+    console.log('Clicked')
+  }
+  const deleteHandler = () =>{
+    console.log('Deleted')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -13,6 +19,9 @@ function ExpenseItem(props) {
         <h3>{props.title}</h3>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick = {clickHandeler}>Change Title</button><br/>
+      <button onClick = {deleteHandler}>Delete</button>
+      
     </Card>
   );
 }
