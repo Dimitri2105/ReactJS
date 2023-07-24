50c4b317a275a9765cfcb846d7663b391dcee3f2
Dimitri2105 <karanthakur577@gmail.com>
2023-07-24 08:44:27 +0530

    Lets introduce state inside the new Component

--- src/components/UserExpenses/ExpenseForm.js
+++ src/components/UserExpenses/ExpenseForm.js
@@ -8,6 +8,14 @@ function ExpenseForm() {
         console.log('title changes.....')
         console.log(event.target.value)
     }
+    const amountChangeHandler = (event) =>{
+      console.log('Amount Changed')
+      console.log(event.target.value)
+    }
+    const dateChangeHandler = (event) =>{
+      console.log('Date Changed')
+      console.log(event.target.value)
+    }
   return (
     <form>
       <div className="new-expense__controls">
@@ -17,11 +25,11 @@ function ExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Expense Amount</label>
-          <input type="number"/>
+          <input type="number" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date"/>
+          <input type="date" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
