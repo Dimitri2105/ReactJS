215cbb497ef784a1e89a7602cb874d4052b9b7dc
Dimitri2105 <karanthakur577@gmail.com>
2023-07-30 08:42:00 +0530

    Rendering Expenses with performance optimisations

--- src/components/UserExpenses/NewExpense.js
+++ src/components/UserExpenses/NewExpense.js
@@ -3,10 +3,20 @@ import React from "react";
 import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 
-function NewExpense() {
+function NewExpense(props) {
+  const saveExpenseHandler = (enteredExpenseData) =>{
+    const expenseData = {
+      ...enteredExpenseData,
+      id : Math.random().toString()
+
+    }
+    console.log(expenseData)
+    props.onAddExpense(expenseData)
+
+  }
   return (
     <div className="new-expense">
-        <ExpenseForm/>
+        <ExpenseForm onSaveExpenseData = {saveExpenseHandler}/>
     </div>
   );
 }
